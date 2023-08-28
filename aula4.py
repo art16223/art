ffc0e71e21f532cba083cfad80fcfa4e87d0b21e
art16223 <143456567+art16223@users.noreply.github.com>
2023-08-28 19:02:46 -0300

    Add files via upload
    aula4

---
+++ aula4.py
@@ -0,0 +1,19 @@
+'''def aula():
+    print("conchinha calvo")
+
+aula()'''
+
+'''def saudacao(nome):
+    print(" olá " + nome + " seja bem vindo! ")
+
+saudacao("arthur")'''
+
+def mult(a, b):
+    return a * b
+
+v1 = int(input("Digte o primeiro valor"))
+v2 = int(input("Digte o segundo valor"))
+
+resultado = mult(v1, v2)
+
+print(resultado)
